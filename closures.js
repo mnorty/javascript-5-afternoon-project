fce114369382d7cd2cc267cdf3e96b45887d5b37
mnorty <mnorty@gmail.com>
2019-05-28 14:55:38 -0600

    closures done

--- closures.js
+++ closures.js
@@ -217,10 +217,11 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
   }
+  
 }
 timeOutCounter();
